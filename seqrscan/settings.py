ec7ce76cde8781a33360176bbb151a9f8b6256fc
Mark Gabriel <markgabrielquinto0@gmail.com>
2023-10-03 11:49:54 +0800

    Updated

--- seqrscan/settings.py
+++ seqrscan/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = "django-insecure-e&4pexyqj$4y+)+9+=wf+k_e-x@4*7%c=_*vgq2u!olv^$vt39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    '192.168.1.16',
-    "http://192.168.1.16:8000/"
-    ]
+ALLOWED_HOSTS = []
 CORS_ORIGIN_ALLOW_ALL = True 
 
 # Application definition
