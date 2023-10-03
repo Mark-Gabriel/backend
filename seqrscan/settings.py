5b3ab95e4b4242b1e9fd7b1d37b8c8b9488fae8e
Mark Gabriel <markgabrielquinto0@gmail.com>
2023-10-03 16:50:21 +0800

    Updated

--- seqrscan/settings.py
+++ seqrscan/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["3.27.212.25"]
 CORS_ORIGIN_ALLOW_ALL = True 
-
+CORS_ALLOW_CREDENTIALS = False
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
 # Application definition
 
 INSTALLED_APPS = [
