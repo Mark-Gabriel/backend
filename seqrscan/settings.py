fc0340abc75cf3564f2e7858862578776cd4bd17
Mark Gabriel <markgabrielquinto0@gmail.com>
2023-10-03 16:58:56 +0800

    Updated

--- seqrscan/settings.py
+++ seqrscan/settings.py
@@ -28,9 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["3.27.212.25"]
 CORS_ORIGIN_ALLOW_ALL = True 
 CORS_ALLOW_CREDENTIALS = False
-CORS_ORIGIN_WHITELIST = (
-    '*'
-)
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -53,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
